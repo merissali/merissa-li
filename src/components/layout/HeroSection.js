3cfead531a78a7e3e507a343a73d3b922ad3d2b1
Merissa Li <merissajli@gmail.com>
2021-08-07 19:06:19 -0700

    Added work section & footer

--- src/components/layout/HeroSection.js
+++ src/components/layout/HeroSection.js
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
-import homeBg from "../img/homeBg.jpg";
-import { Button } from "./Button";
+import homeBg from "../../img/homeBg.jpg";
+import { Button } from "../Button";
 
 function HeroSection() {
   return (
     <HeroContainer id='home'>
       <HeroBg>
         <PictureBg src={homeBg}></PictureBg>
+        <Overlay />
       </HeroBg>
       <HeroContent>
         <HeroH1>Hi, I'm Merissa!</HeroH1>
@@ -44,6 +45,16 @@ const HeroBg = styled.div`
   overflow: hidden;
 `;
 
+const Overlay = styled.div`
+  position: absolute;
+  bottom: 0;
+  background: #18191c;
+  width: 100%;
+  height: 100%;
+  opacity: 0.57;
+  z-index: 0;
+`;
+
 const PictureBg = styled.img`
   width: 100%;
   height: 100%;
@@ -66,8 +77,8 @@ const HeroContent = styled.div`
 
 const HeroH1 = styled.h1`
   color: #fff;
-  font-size: 70px;
-  margin-top: 35px;
+  font-size: 80px;
+  margin-top: 250px;
 
   @media screen and (max-width: 768px) {
     font-size: 40px;
@@ -79,7 +90,7 @@ const HeroH1 = styled.h1`
 `;
 
 const HeroBtnWrapper = styled.div`
-  margin-top: 60px;
+  margin-top: 70px;
   display: flex;
   flex-direction: column;
   align-items: center;
